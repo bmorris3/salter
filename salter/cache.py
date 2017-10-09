cc2172fa36d6bedf778e78c45dd31161f3516574
Brett Morris <brettmorris21@gmail.com>
2017-10-09 16:16:41 -0700

    Adding joined table machinery

--- salter/cache.py
+++ salter/cache.py
@@ -8,12 +8,17 @@ from astropy.io import ascii
 import os
 import h5py
 from astropy.utils.console import ProgressBar
+from astropy.utils.data import download_file
+from astropy.table import join
+from astropy.table import Column
 
-__all__ = ['get_light_curves']
+__all__ = ['get_light_curves', 'get_joined_table', 'cache_joined_table']
 
 kic_numbers_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                 os.path.pardir, 'data', 'kics.csv')
 
+joined_table_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                os.path.pardir, 'data', 'joined_table.csv')
 
 def get_light_curves():
     kics = ascii.read(kic_numbers_path, format='no_header')['col1']
@@ -50,3 +55,28 @@ def get_light_curves():
                 f.create_dataset(str(kic), data=data)
                 f.flush()
                 bar.update()
+
+
+def cache_joined_table():
+    """
+    Cache a joined table containing data from the NASA Exoplanet Archive and
+    the Exoplanet Orbit Database.
+
+    To get the table, run the ``get_joined_table()`` function
+    """
+    NEA_URL = 'https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative'
+    EOD_URL = 'http://exoplanets.org/csv-files/exoplanets.csv'
+    nea_table = ascii.read(download_file(NEA_URL, cache=False))
+    eod_table = ascii.read(download_file(EOD_URL, cache=False))
+    eod_table2 = eod_table[~eod_table['KEPID'].mask]
+    nea_table2 = nea_table[~nea_table['kepid'].mask]
+
+    eod_table2.add_column(Column(eod_table2['KEPID'], 'kepid'))
+
+    joined_table = join(eod_table2, nea_table2, keys=['kepid'])
+
+    ascii.write(joined_table, joined_table_path, format='csv')
+
+
+def get_joined_table():
+    return ascii.read(joined_table_path, format='csv')
