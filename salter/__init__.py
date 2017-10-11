27b4fad230d05be38b2265347e852c0c07811409
Brett Morris <brettmorris21@gmail.com>
2017-10-10 17:03:12 -0700

    Adding package template

--- salter/__init__.py
+++ salter/__init__.py
@@ -1,8 +1,12 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
 
-from .lightcurve import *
-from .cache import *
-from .params import *
-from .limbdarkening import *
-from .stats import *
+if not _ASTROPY_SETUP_:
+        # For egg_info test builds to pass, put package imports here.
+    from .lightcurve import *
+    from .cache import *
+    from .params import *
+    from .limbdarkening import *
+    from .stats import *
