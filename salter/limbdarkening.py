b0b6d2407a7728694e93520da18600de3c75b1e2
Brett Morris <brettmorris21@gmail.com>
2017-10-11 09:54:40 -0700

    Adding lc fitting methods

--- salter/limbdarkening.py
+++ salter/limbdarkening.py
@@ -5,7 +5,7 @@ from __future__ import (absolute_import, division, print_function,
 import numpy as np
 from astroquery.vizier import Vizier
 
-__all__ = ['quad', 'ld_table']
+__all__ = ['quad', 'ld_table', 'q2u', 'u2q']
 
 
 class LDTable(object):
@@ -27,7 +27,7 @@ ld_table = LDTable()
 
 
 def _get_closest_model(Teff, logg, filt):
-    '''
+    """
     All grid points have metallicities Z=0 (solar), microturbulence xi=2
 
     Parameters
@@ -40,7 +40,7 @@ def _get_closest_model(Teff, logg, filt):
 
     Teff : float
         Host star effective temperature.
-    '''
+    """
     correct_filter = ld_table.table['Filt'] == filt
     method = ld_table.table['Met'] == 'F'
     mask = (correct_filter & method)
@@ -80,3 +80,63 @@ def quad(Teff, logg, filt):
         closestmodel = _get_closest_model(5800, logg, filt)
 
     return ld_table.table['a'][closestmodel], ld_table.table['b'][closestmodel]
+
+
+def u2q(u1, u2, warnings=True):
+    """
+    Convert the linear and quadratic terms of the quadratic limb-darkening
+    parameterization -- called `u_1` and `u_2` in Kipping 2013 or `a` and `b` in
+    Claret et al. 2013 -- and convert them to `q_1` and `q_2` as described in
+    Kipping 2013:
+
+    http://adsabs.harvard.edu/abs/2013MNRAS.435.2152K
+
+    Parameters
+    ----------
+    u1 : float
+        Linear component of quadratic limb-darkening
+
+    u2 : float
+        Quadratic component of quadratic limb-darkening
+
+    Returns
+    -------
+    (q1, q2) : tuple of floats
+        Kipping (2013) style quadratic limb-darkening parameters
+    """
+    q1 = (u1 + u2)**2
+    q2 = 0.5*u1/(u1+u2)
+    if warnings and (u1 < 0 or u2 < 0):
+        print("WARNING: The quadratic limb-darkening parameters " +
+              "u1={0:.3f} or u2={0:.3f} violate Kipping's ".format(u1, u2) +
+              "conditions for a monotonically increasing or everywhere-" +
+              "positive intensity profile. Returning them as is.")
+    return q1, q2
+
+
+def q2u(q1, q2):
+    """
+    Convert the two parameter quadratic terms of the Kipping 2013 limb-
+    darkening parameterization `q_1` and `q_2` to the standard linear and
+    quadratic terms of the quadratic limb-darkening parameterization of
+    Claret et al. 2013 -- called `u_1` and `u_2` in Kipping 2013 or `a` and `b` in
+    Claret et al. 2013:
+
+    http://adsabs.harvard.edu/abs/2013A%26A...552A..16C
+
+    Parameters
+    ----------
+    q1 : float
+        First component of Kipping 2013 quadratic limb-darkening
+
+    q2 : float
+        Second component of Kipping 2013 quadratic limb-darkening
+
+    Returns
+    -------
+    (u1, u2) : tuple of floats
+        Claret et al. (2013) style quadratic limb-darkening parameters
+    """
+    u1 = 2*np.sqrt(q1)*q2
+    u2 = np.sqrt(q1)*(1-2*q2)
+    return u1, u2
